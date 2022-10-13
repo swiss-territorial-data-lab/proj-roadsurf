60d818772a9469e0c2fb1905cb0d1ce619cbf908
Gwenaëlle S <gwenaelle.salamin@gmail.com>
2022-10-13 10:55:39 +0000

    final corrections post 1st run
    Moving the functions for statistics in a new file, renaming the files for the funcitons, setting margins for the graphs

--- 03_Scripts/fct_misc.py
+++ 03_Scripts/fct_misc.py
@@ -1,11 +1,16 @@
 import sys
 import os
 
+import geopandas as gpd
+
 def test_crs(crs1, crs2 = "EPSG:2056"):
     '''
     Take the crs of two dataframes and compare them. If they are not the same, stop the script.
     '''
-
+    if isinstance(crs1, gpd.GeoDataFrame):
+        crs1=crs1.crs
+    if isinstance(crs2, gpd.GeoDataFrame):
+        crs2=crs2.crs
 
     try:
         assert(crs1 == crs2), "CRS mismatch between the two files."
